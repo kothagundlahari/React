a67f8267f4ac379efa972450f75e4af5b71234a1
hari krisha <hari.k.krishna4@gmail.com>
2017-01-12 06:56:25 -0600

    created my first component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
-const App = () => < h1 > Hello!!and hip hip hurray!! < /h1>
+//const App = () => < h1 > Hello!!and hip hip hurray!! < /h1>
+
+class App extends React.Component {
+    render() {
+        return <h1 > Hello this is my first component < /h1>
+
+    }
+}
 
 
 
